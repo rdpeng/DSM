eb5133981a175cb6228d002c1e2787a1a93dc7b0
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-06-25 07:41:50 -0400

    Start some classes and methods for dsm

---
+++ R/datastructures.R
@@ -0,0 +1,87 @@
+################################################################################
+## Data structures for environmental epi example
+################################################################################
+
+setGeneric("units")
+setGeneric("units<-")
+
+setOldClass("lm")
+setOldClass("glm")
+
+setClass("modelOutput",
+         representation(model = "lm",
+                        exposure = "character",
+                        other = "character"))
+
+setGeneric("getNames", function(x, ...) standardGeneric("getNames"))
+setMethod("getNames", "modelOutput",
+          function(x, ...) {
+                  frm <- formula(x@model)
+                  list(response = as.character(frm)[2],
+                       other = attr(terms(x@model), "term.labels"))
+          })
+
+char2comma <- function(x) {
+        n <- length(x)
+        if(n <= 1L)
+                x
+        else if(n == 2L)
+                paste(sQuote(x), collapse = " and ")
+        else
+                paste0(paste(sQuote(x[-n]), collapse = ", "), ", and ",
+                       sQuote(x[n]))
+}
+
+setGeneric("modelType", function(object, ...) standardGeneric("modelType"))
+
+setMethod("modelType", "lm",
+          function(object, ...) {
+                  "linear regression"
+          })
+
+setMethod("modelType", "glm",
+          function(object, ...) {
+                  fam <- family(object)
+                  if(fam$family == "gaussian")
+                          "linear regression"
+                  else if(fam$family == "binomial") {
+                          paste(fam$link, "regression")
+                  }
+                  else 
+                          paste(fam$family, "regression")                  
+          })
+
+setGeneric("describeModel", function(object, ...)
+           standardGeneric("describeModel"))
+
+setMethod("describeModel", "modelOutput",
+          function(object, ...) {
+                  nms <- getNames(object)
+                  sprintf("in this %s model, %s was regressed on %s",
+                          modelType(object@model), sQuote(nms$response),
+                          char2comma(nms$other))
+          })
+
+setGeneric("modelEffect", function(object, ...) standardGeneric("modelEffect"))
+
+setMethod("modelEffect", "modelOutput",
+          function(object, incr = 1, ...) {
+                  b <- coef(object@model)[object@exposure]
+                  bb <- b * incr
+                  sprintf("a %s unit %s in %s was associated with a %s unit %s in %s",
+                          abs(incr), ifelse(incr > 0, "increase", "decrease"),
+                          sQuote(object@exposure), prettyNum(abs(bb)),
+                          ifelse(bb > 0, "increase", "decrease"),
+                          sQuote(getNames(object)$response))
+          })
+
+startcaps <- function(x) {
+
+}
+
+setMethod("show", "modelOutput",
+          function(object) {
+                  out <- describeModel(object)
+                  cat(out, ".\n", sep = "")
+                  invisible(object)
+          })
