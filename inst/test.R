e0ea7fb29b5d479a054d5391e69bfff97764583d
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-06-28 17:03:12 -0400

    Run the full pipeline; draft

--- inst/test.R
+++ inst/test.R
@@ -1,16 +1,15 @@
-library(splines)
-f <- lm(Ozone ~ Solar.R + Wind + ns(Temp, 2), data = airquality)
-m <- new("exposureModel", model = f, exposure = "Wind")
-
-
 library(tsModel)
 library(splines)
 source("R/datastructures.R")
-## d0 <- readAPTSData("inst/ny.rds")
-d0 <- readAPTSData("inst/chic.rds")
+## d0 <- readAPTSData("inst/chic.rds")
+d0 <- readAPTSData("inst/ny.rds")
 ch <- checkData(d0)
-g <- glm(death ~ dow + ns(date, 19 * 6) + ns(tmpd, 6) + ns(rmtmpd, 6) + ns(dptp, 3) + ns(rmdptp, 3) + Lag(pm10tmean, 1), data = as(d0, "data.frame"), family = poisson)
+base <- glm(death ~ dow + ns(date, 19 * 6) + ns(tmpd, 6) + ns(rmtmpd, 6) + ns(dptp, 3) + ns(rmdptp, 3) + Lag(pm10tmean, 1), data = as(d0, "data.frame"), family = poisson)
+ap <- new("APTSModel", timevar = "date", model = base, exposure = "pm10tmean", summary = ch)
+dfmax <- selectTimeDF(ap)
+
+g <- glm(death ~ dow + ns(date, 19 * dfmax) + ns(tmpd, 6) + ns(rmtmpd, 6) + ns(dptp, 3) + ns(rmdptp, 3) + Lag(pm10tmean, 1), data = as(d0, "data.frame"), family = poisson)
 ap <- new("APTSModel", timevar = "date", model = g, exposure = "pm10tmean", summary = ch)
 
-sens <- sensitivityAnalysis(ap, 19 * 2:12)
-
+sens <- sensitivityAnalysis(ap, 19 * 4:20)
+infl <- influenceAnalysis(ap)
