996919ba04af42b289762834f13042f943fa545c
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-07-09 16:21:25 -0400

    Start code for populating Rmd template

---
+++ R/codewriter.R
@@ -0,0 +1,8 @@
+################################################################################
+## Write out options to template Rmd file
+################################################################################
+
+analysisAPTS <- function(datafile, exposure = NULL, response = NULL,
+                         dfseq = 4:20) {
+
+}
