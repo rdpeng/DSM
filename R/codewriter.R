3e1333e78ce79c25691f2244dd1b2af0ad63e70c
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-07-16 09:53:32 -0400

    Use writeInput Parameters function

--- R/codewriter.R
+++ R/codewriter.R
@@ -2,21 +2,40 @@
 ## Write out options to template Rmd file
 ################################################################################
 
+makeList <- function(...) {
+        args <- substitute(list(...))
+        nms <- sapply(args[-1], deparse)
+        vals <- list(...)
+        names(vals) <- nms
+        vals
+}
+
 analysisAPTS <- function(datafile, exposure = NULL, response = NULL,
                          timevar = NULL) {
+        writeInputParameters(datafile, exposure, response, timevar)
+}
+
+writeInputParameters <- function(...) {
+        vals <- makeList(...)
+        parnames <- names(vals)
+
         con <- file("input_parameters.Rmd", "w")
         on.exit(close(con))
         op <- options()
         options(useFancyQuotes = FALSE)
         on.exit(options(op), add = TRUE)
         writeLines("```{r input parameters}", con)
-        writeLines(sprintf("datafile <- %s", dQuote(datafile)), con)
-        writeLines(sprintf("exposure <- %s", ifelse(is.null(exposure), "NULL", dQuote(exposure))), con)
-        writeLines(sprintf("response <- %s", ifelse(is.null(response), "NULL", dQuote(response))), con)
-        writeLines(sprintf("timevar <- %s", ifelse(is.null(timevar), "NULL", dQuote(timevar))), con)
+        for(i in seq_along(vals)) {
+                if(is.null(vals[[i]]))
+                        msg <- sprintf("%s <- NULL", parnames[i])
+                else if(is.character(vals[[i]]))
+                        msg <- sprintf("%s <- \"%s\"", parnames[i], vals[[i]])
+                else if(is.numeric(vals[[i]]))
+                        msg <- sprintf("%s <- %f", parnames[i], vals[[i]])
+                else 
+                        msg <- sprintf("%s <- %s", parnames[i],
+                                       as.character(vals[[i]]))
+                writeLines(msg, con)
+        }
         writeLines("```", con)
-}
-
-writeInputParameters <- function(paramlist) {
-
 }
