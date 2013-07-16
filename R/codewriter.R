798af84b563896b79a6825eabb94abbcdecd2fb5
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-07-16 09:20:17 -0400

    Write out input parameter file

--- R/codewriter.R
+++ R/codewriter.R
@@ -3,6 +3,20 @@
 ################################################################################
 
 analysisAPTS <- function(datafile, exposure = NULL, response = NULL,
-                         dfseq = 4:20) {
+                         timevar = NULL) {
+        con <- file("input_parameters.Rmd", "w")
+        on.exit(close(con))
+        op <- options()
+        options(useFancyQuotes = FALSE)
+        on.exit(options(op), add = TRUE)
+        writeLines("```{r input parameters}", con)
+        writeLines(sprintf("datafile <- %s", dQuote(datafile)), con)
+        writeLines(sprintf("exposure <- %s", ifelse(is.null(exposure), "NULL", dQuote(exposure))), con)
+        writeLines(sprintf("response <- %s", ifelse(is.null(response), "NULL", dQuote(response))), con)
+        writeLines(sprintf("timevar <- %s", ifelse(is.null(timevar), "NULL", dQuote(timevar))), con)
+        writeLines("```", con)
+}
+
+writeInputParameters <- function(paramlist) {
 
 }
